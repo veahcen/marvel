7c357823550a15d79d0cfd1badd14740e3ffee99
Mihail <liahim.veahcen@gmail.com>
2024-10-02 15:53:05 +0300

    made a pagination and some fix

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,7 +3,7 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from '../../errorBoundary/ErrorBoundary';
+import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 
 import decoration from '../../resources/img/vision.png';
 
